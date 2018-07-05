618c65bee251ce37614675c35f649c9b571033e7
unknown <andiyeska@gmail.com>
2018-07-05 12:20:35 +0700

    init

---
+++ src/main/java/com/event/spare/SpareApplication.java
@@ -0,0 +1,12 @@
+package com.event.spare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpareApplication.class, args);
+	}
+}
---
+++ src/main/java/com/event/spare/controller/EodController.java
@@ -0,0 +1,15 @@
+package com.event.spare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/eod")
+public class EodController {
+
+	@RequestMapping(value = {"", "/", "/index"})
+	public String index() {
+		return "eod-index";
+	}
+	
+}
